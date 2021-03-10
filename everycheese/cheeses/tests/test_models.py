b71b38fdc86257b604d4e8f16dc8d6a361900279
Sungsoon Park <sungsoon.park@gmail.com>
2021-03-10 11:44:23 -0800

    Use CheeseFactory in a test, but FAILED. Not figured out.

---
+++ everycheese/cheeses/tests/test_models.py
@@ -0,0 +1,15 @@
+# from ..models import Cheese
+from .factories import CheeseFactory
+
+
+# cheese = Cheese.objects.create(
+#     name='Stracchino',
+#     description='Semi-Sweet Cheese that goes well with starches',
+#     firmness=Cheese.Firmness.SOFT,
+# )
+# cheese = CheeseFactory(name='Stracchino')
+
+def test__str__():
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
